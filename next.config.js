d60dd55d874a0ac7501f3366e2654f814abb582c
Rahul Patwa <rahul.patwa@automationedge.com>
2024-01-25 17:54:56 +0530

    env for vercel

--- next.config.js
+++ next.config.js
@@ -2,3 +2,7 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(process.cwd(),'.vercel/env.production.local') });
